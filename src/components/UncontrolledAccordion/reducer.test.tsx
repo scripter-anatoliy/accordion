3bcd86b0e0afadcfdc1056869ab26868cf1101c4
Anatoly <anatoly.selivanov1@gmail.com>
2021-08-22 14:05:04 +0300

    15 - test on reducer, refactoring comments

--- src/components/UncontrolledAccordion/reducer.test.tsx
+++ src/components/UncontrolledAccordion/reducer.test.tsx
@@ -14,28 +14,24 @@ test("collapsed should be true", () => {
 })
 
 test("collapsed should be false", () => {
-    // 3 части теста
-    // data - данные
+
     const state: StateType = {
         collapsed: true
     }
-    // action
+
     const newState = reducer(state, {type: TOGGLE_CONSTANT})
 
-    //expect - результат
     expect(newState.collapsed).toBe(false)
 })
 
 test("collapsed should be error", () => {
-    // 3 части теста
-    // data - данные
+
     const state: StateType = {
         collapsed: true
     }
-    // action
+
     expect(() => {
         reducer(state, {type: "FAKETYPE"})
     }).toThrowError()
-
 
 })
