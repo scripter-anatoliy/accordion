f664e1df3e3fec500b0f9e0798202f663e6d653b
Anatoly <anatoly.selivanov1@gmail.com>
2021-09-07 18:18:38 +0300

    21. AnalogClock refactoring

--- src/components/Clock/AnalogClock.tsx
+++ src/components/Clock/AnalogClock.tsx
@@ -6,56 +6,50 @@ import {ClockDataType} from "./ClockDivided";
 
 export const AnalogClock: React.FC<ClockDataType> = ({date}) => {
     console.log("analog")
+
     const deg = 6;
-    const hr = document.querySelector('#hr');
-    const sc = document.querySelector('#sc');
+    const hr = document.querySelector('#hr') as HTMLStyleElement;
+    const mn = document.querySelector('#mn') as HTMLStyleElement;
+    const sc = document.querySelector('#sc') as HTMLStyleElement;
 
-    setInterval(() => {
-        let date = new Date()
-        let hh = date.getHours() * 30;
-        let mm = date.getMinutes() * deg;
-        let ss = date.getSeconds() * deg;
-        // @ts-ignore
-        hr.style.transform = `rotateZ(${(hh) + (mm/12)}deg)`;
-        // @ts-ignore
+
+    console.log('date')
+    let hh = date.getHours() * 30;
+    let mm = date.getMinutes() * deg;
+    let ss = date.getSeconds() * deg;
+
+    if (hr !== null) {
+        hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+    }
+    if (mn !== null) {
         mn.style.transform = `rotateZ(${mm}deg)`;
-        // @ts-ignore
+    }
+    if (sc !== null) {
         sc.style.transform = `rotateZ(${ss}deg)`;
-    })
+    }
 
-        // const hrStyle = {
-        //     transform: `rotateZ(${(date.getHours() * 30) + (date.getMinutes() / 12)}deg)`
-        // }
-        // const mnStyle = {
-        //     transform: `rotateZ(${date.getMinutes() * 6}deg)`
-        // }
-        // const scStyle = {
-        //     transform: `rotateZ(${date.getSeconds() * 6}deg)`
-        // }
-
-
-        return <div className={style.body}>
-            <div className={style.clock}>
-                <img src={clockFace} alt=""/>
-                <img src={clockJs} alt=""/>
-                <div className={style.hour}>
-                    <div className={style.hr}
-                         id="hr">
-                    </div>
+    return <div>
+        <div className={style.clock}>
+            <img src={clockFace} alt=""/>
+            <img src={clockJs} alt=""/>
+            <div className={style.hour}>
+                <div className={style.hr}
+                     id="hr">
                 </div>
-                <div className={style.min}>
-                    <div className={style.mn}
-                         id="mn">
-                    </div>
+            </div>
+            <div className={style.min}>
+                <div className={style.mn}
+                     id="mn">
                 </div>
-                <div className={style.sec}>
-                    <div className={style.sc}
-                         id="sc">
-                    </div>
+            </div>
+            <div className={style.sec}>
+                <div className={style.sc}
+                     id="sc">
                 </div>
             </div>
         </div>
-    }
+    </div>
+}
 
 
 
