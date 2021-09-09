d23961da5088e26f011893ac5f01f2f785ad15b2
Anatoly <anatoly.selivanov1@gmail.com>
2021-09-09 13:30:31 +0300

    refactor project App

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,11 @@ import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOf
 function App() {
     console.log('App rendering')
 
-    //let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
     let [switchOn, setSwitchOn] = useState(false);
-    //let [accordionCollapsedValue, setAccordionCollapsed] = useState(false);
 
     return (
         <div className={'App'}>
-            {/*<PageTitle title={"Good Moning"}/>*/}
-            {/*<PageTitle title={"This is APP component"}/>*/}
-            {/*<Rating value={ratingValue}*/}
-            {/*        onClick={setRatingValue}/>*/}
             <UncontrolledOnOff onChange={setSwitchOn}/> {switchOn.toString()}
-            {/*<OnOff  on={switchOn} onChange={setSwitchOn}/>*/}
-            {/*<Accordion titleValue={"Меню"}*/}
-            {/*           collapsed={accordionCollapsedValue}*/}
-            {/*           onChange={() => {*/}
-            {/*               setAccordionCollapsed(!accordionCollapsedValue)*/}
-            {/*           }}*/}
-            {/*/>*/}
-            {/*<UncontrolledRating  />*/}
-            {/*<UncontrolledAccordion titleValue={"Меню"} />*/}
-            {/*<UncontrolledAccordion titleValue={"Супер Меню"} />*/}
-
         </div>
     );
 }
